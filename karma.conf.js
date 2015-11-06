b59d3c4ac2981f13e5dd1499bb8023371401fe16
parechay <mk.parechay@gmail.com>
2015-11-06 15:14:12 -0500

    this is folder structure

--- karma.conf.js
+++ karma.conf.js
@@ -15,16 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/',
-      '""',
-      'test/*.js',
-      '""',
-      'test/**/*Spec.js',
-      '""',
-      'test/**/*Spec.js',
-      'test/**/*Spec.js',
-      'test/**/*Spec.js',
-      '""'
+       'test/**/*Spec.js'
     ],
 
 
